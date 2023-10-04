5ce85d77bc90fd908a7cc3ea621899b5b020c25e
Alejandro2097 <alejo12381@hotmail.es>
2023-10-03 21:06:39 -0500

    initial state

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 
-import App from './App.jsx'
-import { store } from './store/store.js'
+import App from './App.jsx';
+import { store } from './store';
 
-import './index.css'
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
